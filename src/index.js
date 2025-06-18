16893d5bc33fccbc6559ca88cb9c211b81672d2c
eugeny11 <eugeny11@mail.ru>
2025-06-18 17:40:48 +0200

    modify

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const app = document.getElementById("app");
-if (app) {
-  const root = createRoot(app);
-  root.render(<App />);
-}
+document.addEventListener("DOMContentLoaded", () => {
+  const app = document.getElementById("app");
+  if (app) {
+    const root = createRoot(app);
+    root.render(<App />);
+  }
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
