0b67c10f5cd42bad4209634352f62a73fd8555a0
Eugeny Iakovlev <53447749+eugeny11@users.noreply.github.com>
2025-07-10 17:17:38 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-window.mountApp = function(targetId = "app") {
+/* window.mountApp = function(targetId = "app") {
   const container = document.getElementById(targetId);
   if (container) {
     const root = createRoot(container);
     root.render(<App />);
   } 
-};
+}; */
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
